4c34377cf6f360bac5349c66218035da7ef530d8
Slavica Uljar <slavicauljar@gmail.com>
2016-08-18 11:39:20 +0200

    prvi inicijalni commit

---
+++ IndexTest/test2.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="test2.asmx.cs" Class="IndexTest.test2" %>
---
+++ Travel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel.Web;
+using System.ServiceModel;
+
+namespace Travel
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8000/");
+
+            WebServiceHost svcHost = new WebServiceHost(typeof(Travel.ITravelService), baseAddress);
+
+            try
+            {
+                svcHost.Open();
+
+                Console.WriteLine("Service is running");
+                Console.WriteLine("Press enter to quit...");
+                Console.ReadLine();
+
+                svcHost.Close();
+            }
+            catch (CommunicationException cex)
+            {
+                Console.WriteLine("An exception occurred: {0}", cex.Message);
+                svcHost.Abort();
+            }
+        }
+    }
+}
